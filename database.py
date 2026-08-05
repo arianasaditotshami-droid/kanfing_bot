8f4fec347da312cbe7552b868b12cbec1710515b
arianasaditotshami-droid <arianasaditotshami@gmail.com>
2026-08-05 12:12:15 +0330

    Add orders table and related functions
    Added a new table for orders and functions to save orders and retrieve user orders.

--- database.py
+++ database.py
@@ -3,12 +3,14 @@ import sqlite3
 db = sqlite3.connect("bot.db")
 cursor = db.cursor()
 
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users(
 id INTEGER PRIMARY KEY,
 username TEXT
 )
 """)
+
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS configs(
@@ -18,7 +20,19 @@ config TEXT
 )
 """)
 
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS orders(
+id INTEGER PRIMARY KEY,
+user_id INTEGER,
+config TEXT,
+status TEXT
+)
+""")
+
+
 db.commit()
+
 
 
 def add_user(user_id, username):
@@ -29,9 +43,28 @@ def add_user(user_id, username):
     db.commit()
 
 
+
 def save_config(user_id, text):
     cursor.execute(
         "INSERT INTO configs(user_id,config) VALUES (?,?)",
         (user_id,text)
     )
     db.commit()
+
+
+
+def save_order(user_id, config):
+    cursor.execute(
+        "INSERT INTO orders(user_id,config,status) VALUES (?,?,?)",
+        (user_id, config, "در انتظار پرداخت")
+    )
+    db.commit()
+
+
+
+def get_user_orders(user_id):
+    cursor.execute(
+        "SELECT config,status FROM orders WHERE user_id=?",
+        (user_id,)
+    )
+    return cursor.fetchall()
