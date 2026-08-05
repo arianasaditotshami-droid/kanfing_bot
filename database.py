ddb283f084ed1af946ebdec1ca60ff5b67a4443e
arianasaditotshami-droid <arianasaditotshami@gmail.com>
2026-08-05 14:51:02 +0330

    Create admins and gifts tables; refactor functions
    Added tables for admins and gifts, refactored functions to improve organization and clarity.

--- database.py
+++ database.py
@@ -25,6 +25,22 @@ receipt TEXT
 """)
 
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS admins(
+id INTEGER PRIMARY KEY
+)
+""")
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS gifts(
+code TEXT PRIMARY KEY,
+points INTEGER,
+uses INTEGER DEFAULT 0
+)
+""")
+
+
 db.commit()
 
 
@@ -37,46 +53,12 @@ def add_user(user_id, username, referrer=0):
     )
 
     if referrer and referrer != user_id:
-
         cursor.execute(
             "UPDATE users SET points = points + 3 WHERE id=?",
             (referrer,)
         )
 
     db.commit()
-
-
-
-def save_order(user_id, config):
-
-    cursor.execute(
-        "INSERT INTO orders(user_id,config,status,receipt) VALUES (?,?,?,?)",
-        (user_id, config, "در انتظار پرداخت", "")
-    )
-
-    db.commit()
-
-
-
-def save_receipt(user_id, receipt):
-
-    cursor.execute(
-        "UPDATE orders SET receipt=? WHERE user_id=? ORDER BY id DESC LIMIT 1",
-        (receipt, user_id)
-    )
-
-    db.commit()
-
-
-
-def get_user_orders(user_id):
-
-    cursor.execute(
-        "SELECT config,status FROM orders WHERE user_id=?",
-        (user_id,)
-    )
-
-    return cursor.fetchall()
 
 
 
@@ -93,3 +75,57 @@ def get_points(user_id):
         return result[0]
 
     return 0
+
+
+
+def add_admin(user_id):
+
+    cursor.execute(
+        "INSERT OR IGNORE INTO admins(id) VALUES(?)",
+        (user_id,)
+    )
+
+    db.commit()
+
+
+
+def is_admin(user_id):
+
+    cursor.execute(
+        "SELECT id FROM admins WHERE id=?",
+        (user_id,)
+    )
+
+    return cursor.fetchone() is not None
+
+
+
+def save_order(user_id, config):
+
+    cursor.execute(
+        "INSERT INTO orders(user_id,config,status,receipt) VALUES (?,?,?,?)",
+        (user_id, config, "در انتظار پرداخت", "")
+    )
+
+    db.commit()
+
+
+
+def get_orders():
+
+    cursor.execute(
+        "SELECT * FROM orders"
+    )
+
+    return cursor.fetchall()
+
+
+
+def create_gift(code, points):
+
+    cursor.execute(
+        "INSERT INTO gifts(code,points) VALUES(?,?)",
+        (code, points)
+    )
+
+    db.commit()
